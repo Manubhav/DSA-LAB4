d9724b8357f26a4ddf32f7187a7ffca312e2deba
Manubhav <54354723+Manubhav@users.noreply.github.com>
2019-10-17 14:27:52 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+        struct appliance
+        {
+            char name[20];
+            long long int price,id;
+            float discount;
+        };
+        struct appliance read()
+        {
+            struct appliance a;
+
+                    printf("\nEnter Product name:");
+                    scanf("%s",a.name);
+                    printf("Enter Product id:");
+                    scanf("%lld",&a.id);
+                    printf("Enter Product price:");
+                    scanf("%lld",&a.price);
+                    printf("Enter Product discount:");
+                    scanf("%f",&a.discount);
+
+            return a;	
+        }
+        struct appliance write(struct appliance a)
+            {
+
+                    printf("\nProduct name:%s",a.name);
+                    printf("\nProduct id:%lld",a.id);
+                    printf("\nProduct price:%lld",a.price);
+                    printf("\nProduct discount:%f%",a.discount);
+
+            }
+            main()
+            {
+                struct appliance x[10];
+                int i,n;
+                printf("Enter no. of products:");
+                scanf("%d",&n);
+                for(i=0;i<n;i++)
+                {
+                x[i]=read();
+                }
+                for(i=0;i<n;i++)
+                {
+                write(x[i]);
+                }
+            }
+
+
+
+
